cf60ff8bc15a39c58d2d5f3d67afe91c0909bd96
Erick14911 <erickdlr1945@gmail.com>
2022-08-11 17:25:34 -0500

    11-08-22v1

--- 00_OperacionesAritmeticas.java
+++ 00_OperacionesAritmeticas.java
@@ -1,27 +1,29 @@
+/*@author: Erick14911 
+* Operaciones aritmeticas   
+*/ 
 import java.util.*;
-public class Basico0{
+// Class and main
+public class 00_OperacionesAritmeticas{
     public static void main(String[] args){
         // Your code here!
         Scanner in=new Scanner(System.in);
-        Scanner ca=new Scanner(System.in);
+       
         /*Comentario
         Compuesto*/
         int num1,num2,num3;
-        String nom="Erick",nom2;
+        String nombre="Erick",nombre2;
         double var1,var2,sum,rest,mult,div,exp,mod;
-        Boolean boo=(false);
+        Boolean logico=(false);
 
-        System.out.print("Buenos Días ");
-        System.out.print(nom);
-        System.out.print(" Este programa realiza operaciones entre dos numeros que ingreses.\n");
-        System.out.println("Ingresa el primer numero: ");
+        System.out.println("Buenos Días "+nombre+"\n"+"Este programa realiza operaciones entre dos numeros que ingreses.");
+        System.out.print("Ingresa el primer numero: ");
         num1=in.nextInt();
-        System.out.println("Ingresa el segundo numro: ");
+        System.out.print("\nIngresa el segundo numro: ");
         num2=in.nextInt();
         num3=num1+num2;
-        System.out.print("Ingresa un nombre: ");
-        nom2=ca.nextLine();
-        System.out.println("Hola "+nom2+" tu resultado es: "+num3+" y el booleano es "+boo);
+        System.out.print("\nIngresa un nombre: ");
+        nombre2=in.nextLine();
+        System.out.println("\nHola "+nom2+" tu resultado es: "+num3+" y el booleano es "+logico);
 
         System.out.print("Ingresa el numero A: ");
         var1=in.nextInt();
@@ -33,9 +35,9 @@ public class Basico0{
         div=var1/var2;
         exp=Math.pow(var1,var2);
         mod=var1%var2;
-        //bo=boolean((var1>var2) OR (var1<var2));
-        Boolean bo=(var1==var2);
-        System.out.println("Los numeros son iguales = "+bo);
+       
+        Boolean logico2=(var1==var2);
+        System.out.println("Los numeros son iguales = "+logico2);
         System.out.println(var1+" + "+var2+" = "+sum);
         System.out.println(var1+" - "+var2+" = "+rest);
         System.out.println(var1+" * "+var2+" = "+mult);
